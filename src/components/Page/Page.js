757c2c1da846a8ccafbabdf2952c548beae1598d
Ktan <fredericmamath@gmail.com>
2019-10-17 22:22:25 +0200

    :package: Configure and run ESLint

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import styles from './Page.module.css';
 
-const Page = () => <div className={styles.view}>
-  I'm a page
-</div>;
+const Page = () => 
+    <div className={styles.view}>
+      I&apos;m a page
+    </div>;
 
 export default Page;
