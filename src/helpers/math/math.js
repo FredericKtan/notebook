c28e1f8c6cdd7a5d0dc1b3610bdfba87f846891d
Ktan <fredericmamath@gmail.com>
2019-11-01 23:39:43 +0100

    :construction: Add monkey patching to mock

--- src/helpers/math/math.js
+++ src/helpers/math/math.js
@@ -5,3 +5,13 @@ export const add = (a: number, b: number): number => a + b;
 export const dec = (a: number): number => a - 1;
 
 export const inc = (a: number): number => a + 1;
+
+export const random = (max: number): number =>
+  Math.floor(Math.random() * Math.floor(max));
+
+export default {
+  add,
+  dec,
+  inc,
+  random
+};
