ab55a46314b22d85654f0f50a6c319d8187fd189
Ktan <fredericmamath@gmail.com>
2019-11-07 13:20:20 +0100

    Updates

---
+++ precache-manifest.3b0423e8446ac538999ae97988c57d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07eea1cadc61ed737f9d95c58b3d1ad6",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "5564e7d212b187dfe10f",
+    "url": "/notebook/static/css/main.4c291090.chunk.css"
+  },
+  {
+    "revision": "434890f37e0de0265fd1",
+    "url": "/notebook/static/js/2.082895e2.chunk.js"
+  },
+  {
+    "revision": "5564e7d212b187dfe10f",
+    "url": "/notebook/static/js/main.99f24b02.chunk.js"
+  },
+  {
+    "revision": "61e63760f613ac8eb27d",
+    "url": "/notebook/static/js/runtime-main.c6b1dc33.js"
+  }
+]);
